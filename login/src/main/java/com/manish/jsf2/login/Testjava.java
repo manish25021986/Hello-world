89826f1eacdd8dae0206b4bb32cc5411d19b9a36
Manish <Manish@192.168.2.6>
2015-02-25 22:40:18 +0530

    okkk code deleted.

--- login/src/main/java/com/manish/jsf2/login/Testjava.java
+++ login/src/main/java/com/manish/jsf2/login/Testjava.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "okkkkkkHello World!" );
+        System.out.println( "hello World!" );
     }
 }
